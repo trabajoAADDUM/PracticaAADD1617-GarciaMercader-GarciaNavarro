22217b8e703816ac4c209725c6641175650cc8c5
jesusdemoralla <jesusdemoratalla@hotmail.com>
2016-11-22 19:56:41 +0100

    a

--- src/servlet/FileUploadServlet.java
+++ src/servlet/FileUploadServlet.java
@@ -1,39 +1,75 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FileUploadServlet
- */
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+// A partir de 3.0. Esto evita usar web.xml
 @WebServlet("/FileUploadServlet")
 public class FileUploadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FileUploadServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public FileUploadServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+		if (isMultipart) {
+			FileItemFactory factory = new DiskFileItemFactory();
+
+			ServletFileUpload upload = new ServletFileUpload(factory);
+
+			try {
+				List<FileItem> items = upload.parseRequest(request);
+				Iterator<FileItem> iterator = items.iterator();
+				while (iterator.hasNext()) {
+					FileItem item = (FileItem) iterator.next();
+					if (!item.isFormField()) {
+						String fileName = item.getName();
+						String root = getServletContext().getRealPath("/");
+						System.out.println(root);
+
+						File path = new File(root + "/uploads");
+						System.out.println("**** " + path);
+
+						if (!path.exists()) {
+							path.mkdirs();
+						}
+
+						File uploadedFile = new File(path + "/" + fileName);
+						System.out.println(uploadedFile.getAbsolutePath());
+
+						item.write(uploadedFile);
+					}
+				}
+			} catch (FileUploadException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
